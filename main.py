3a4e754097db9e127b50fc52dd5a1145d6b5d0f8
Nalin Iyer <gigaquake07@gmail.com>
2024-12-03 15:01:48 +0530

    .

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ free()
 
 if day=='monday':
   submon()
+  for i, j in zip(freeclasses, subs):
+    print(j[0], 'in', i)
 elif day=='tuesday':
   subtue()
+  for i, j in zip(freeclasses, subs):
+    print(j[0], 'in', i)
 elif day=='wednesday':
   subwed()
+  for i, j in zip(freeclasses, subs):
+    print(j[0], 'in', i)
 elif day=='thursday':
   subthurs()
+  for i, j in zip(freeclasses, subs):
+    print(j[0], 'in', i)
 elif day=='friday':
   subfri()
-
-for i, j in zip(freeclasses, subs):
-  print(j[0], 'in', i)
+  for i, j in zip(freeclasses, subs):
+    print(j[0], 'in', i)
