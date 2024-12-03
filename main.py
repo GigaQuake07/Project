915dd52516e4374475f4f4f9aec7aaa8ebb473d4
Nalin Iyer <gigaquake07@gmail.com>
2024-12-03 15:15:48 +0530

    .

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from Functions import *
 
-getday()
-attendance()
-free()
+day=input('Enter day: ')
 
 if day=='monday':
   submon()
+  attendance()
+  free()
   for i, j in zip(freeclasses, subs):
     print(j[0], 'in', i)
 elif day=='tuesday':
   subtue()
+  attendance()
+  free()
   for i, j in zip(freeclasses, subs):
     print(j[0], 'in', i)
 elif day=='wednesday':
   subwed()
+  attendance()
+  free()
   for i, j in zip(freeclasses, subs):
     print(j[0], 'in', i)
 elif day=='thursday':
   subthurs()
+  attendance()
+  free()
   for i, j in zip(freeclasses, subs):
     print(j[0], 'in', i)
 elif day=='friday':
   subfri()
+  attendance()
+  free()
   for i, j in zip(freeclasses, subs):
     print(j[0], 'in', i)
